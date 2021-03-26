98f5dfed2c8572e7ce9d48db31938dd04554cdb3
Garrett Brown <garrettpaulbrown@gmail.com>
2021-03-26 15:59:16 -0700

    Update index with html, main.js.

--- src/main.js
+++ src/main.js
@@ -2,3 +2,12 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+// import ExchangeService from './js/exchange-service';
+
+$("#submit").click(function(event){
+    event.preventDefault();
+    let currency = $("#currencyInput").val();
+    $("#currencyInput").val("");
+
+    console.log(currency);
+});
