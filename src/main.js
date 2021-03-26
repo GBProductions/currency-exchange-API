c465462462aafa0fb467ec4381337cb83048e1d0
Garrett Brown <garrettpaulbrown@gmail.com>
2021-03-26 16:08:34 -0700

    Main.js update.

--- src/main.js
+++ src/main.js
@@ -2,12 +2,19 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import ExchangeService from './js/exchange-service';
+import ExchangeService from './js/exchange-service';
+
+function getElements(response){
+    
+}
 
 $("#submit").click(function(event){
     event.preventDefault();
-    let currency = $("#currencyInput").val();
+    let country = $("#currencyInput").val();
     $("#currencyInput").val("");
 
-    console.log(currency);
+    ExchangeService.getExchange(country)
+        .then(function(response) {
+            getElements(response);
+        });
 });
