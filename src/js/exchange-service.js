d4e060bb782da7f766a3b4bc801275c0d37564c2
Garrett Brown <garrettpaulbrown@gmail.com>
2021-03-31 14:15:32 -0700

    Add reset button, delete redundant code in js and html.

--- src/js/exchange-service.js
+++ src/js/exchange-service.js
@@ -5,7 +5,6 @@ export default class ExchangeService {
         return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
             .then(function(response) {
                 if (!response.ok) {
-                    $(".showErrors").text(response.statusText);
                     throw Error(response.statusText);
                 }
                 return response.json();
