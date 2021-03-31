c7793f98c87c842adee531821879d4e356abf59a
Garrett Brown <garrettpaulbrown@gmail.com>
2021-03-31 13:50:08 -0700

    Add context to index.html.

--- src/js/elements.js
+++ src/js/elements.js
@@ -3,12 +3,21 @@ import $ from 'jquery';
 export default class Elements {
     static getElements(response, country, amount){
         if (country === "USD") {
-            $('.showUSD').text(`Why are you trying to convert USD into USD?`);
-        } else if (country !== ("AED" || "AFN" || "ALL" || "AMD" || "ANG")) {
-            $('.showNotACountry').text(`Type in a real country!`);
-        } else if (country === ("AED" || "AFN" || "ALL" || "AMD" || "ANG")) {
+            $(".showUSD").text(`Why are you trying to convert USD into USD?`);
+            $(".showConversion").text("");
+            $(".showErrors").text("");
+            $(".showNotACountry").text("");
+        } else if ((country === "AED") || (country === "AFN") || (country === "ALL") || (country === "AMD") || (country === "ANG")) {
             let final = Math.round((amount * (`${response.conversion_rates[country]}`)));
-            $('.showConversion').text(`$${amount} USD is worth ${final} ${country}!`);
+            $(".showConversion").text(`$${amount} USD is worth ${final} ${country}!`);
+            $(".showUSD").text("");
+            $(".showErrors").text("");
+            $(".showNotACountry").text("");
+        } else {
+            $(".showNotACountry").text(`Type in a real country!`);
+            $(".showUSD").text("");
+            $(".showConversion").text("");
+            $(".showErrors").text("");
         }
     }
 
